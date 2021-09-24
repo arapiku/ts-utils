e528c5f7b976fdc8969d38e1f50f1d0988b5e2fe
arapiku <arapiku420@gmail.com>
2021-09-24 17:50:22 +0900

    feat: add number#format

--- src/number.ts
+++ src/number.ts
@@ -1,3 +1,10 @@
+import { isNil } from 'lodash'
+
+/**
+ * @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+ */
+const formatter = Intl.NumberFormat('ja-JP', { maximumFractionDigits: 3 }).format
+
 /**
  * 数値を桁数に合うようにゼロパディングする
  * @param num 数値
@@ -6,4 +13,13 @@
  */
 export function padZero(num: number | string, length = 2) {
   return !isNaN(Number(num)) ? String(num).padStart(length, '0') : null
+}
+
+/**
+ * 数値をカンマ区切りでフォーマットする
+ * @param num 数値
+ * @returns フォーマットされた文字列
+ */
+export function format(num: number) {
+  return !isNil(num) ? formatter(num) : num
 }
