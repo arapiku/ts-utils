520f2538cd89d332f3513038cedad87ba1915f37
arapiku <arapiku420@gmail.com>
2021-09-25 10:59:52 +0900

    feat: add vue#useClock

--- src/vue.ts
+++ src/vue.ts
@@ -1,4 +1,4 @@
-import { onBeforeMount, onBeforeUnmount, onMounted } from "@vue/composition-api"
+import { onBeforeMount, onBeforeUnmount, onMounted, reactive } from "@vue/composition-api"
 import { throttle } from "lodash"
 
 /**
@@ -11,4 +11,26 @@ export function useWindowResize(listener: () => void, wait = 200) {
   onBeforeMount(() => window.addEventListener('resize', onResize))
   onBeforeUnmount(() => window.removeEventListener('resize', onResize))
   onMounted(() => setTimeout(onResize))
+}
+
+let clock: { nowMinute: number; nowSecond: number }
+
+/**
+ * 毎分毎秒で更新されるタイマーを発行する
+ * @returns 現在秒と現在分のオブジェクト
+ */
+export function useClock() {
+  if (!clock) {
+    clock = reactive({ nowMinute: 0, nowSecond: 0 })
+    const secondMs = 1000
+    const minuteMs = 60000
+    const update = () => {
+      const now = Date.now()
+      clock.nowSecond = Math.floor(now / secondMs) * secondMs
+      clock.nowMinute = Math.floor(now / minuteMs) * minuteMs
+      setTimeout(update, now % secondMs)
+    }
+    update()
+  }
+  return clock
 }
